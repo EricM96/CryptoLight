6a25ac53dcff6b71638ecd42c462d04195eed2cd
Eric McCullough <eric96@live.missouristate.edu>
2019-04-29 21:28:28 -0500

    Removed redundant lines from setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup, Extension
 
 CryptoLight = Extension("CryptoLight", sources=["scratch.cpp"],
-                                       include_dirs=["/usr/local/include"],
+                                       #include_dirs=["/usr/local/include"],
                                        libraries=["cryptopp"],
-                                       runtime_library_dirs=["/usr/local/lib"])
+                                       #runtime_library_dirs=["/usr/local/lib"]
+                                       )
 
 setup(name="CryptoLight",
       version="0.1",
