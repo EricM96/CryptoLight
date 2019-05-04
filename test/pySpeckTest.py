1130df01d886f9eea2d6d61bcd654a07e5d6a821
Eric McCullough <eric96@live.missouristate.edu>
2019-05-03 22:21:15 -0500

    Implement benchmarking

--- test/pySpeckTest.py
+++ test/pySpeckTest.py
@@ -8,6 +8,11 @@ oriented interface for the functions contained in cryptolight.cpp
 import CryptoLightFunctions
 import random, string, time, sys
 
+from base64 import b64encode
+from Cryptodome.Cipher import AES
+from Cryptodome.Random import get_random_bytes
+from Cryptodome.Util.Padding import pad, unpad
+
 class CryptoLight(object):
     def __init__(self, mode):
         """
@@ -39,8 +44,22 @@ class CryptoLight(object):
     def decrypt(self, ciphertext):
         return self.decrypt_function(ciphertext)
 
+class AES_Crypt(object):
+    def __init__(self):
+        self.key = get_random_bytes(16)
 
-def main():
+    def encrypt(self, msg):
+        msg = pad(msg, AES.block_size)
+        iv = get_random_bytes(16)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return iv, cipher.encrypt(msg) 
+
+    def decrypt(self, iv, msg):
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return unpad(cipher.decrypt(msg), AES.block_size)
+
+
+def test():
     while True:
         c = CryptoLight(sys.argv[1])
         letters = string.ascii_lowercase
@@ -54,6 +73,40 @@ def main():
             break
         print(recovered_plaintext)
         time.sleep(1)
-    
+
+def main():
+    # c = CryptoLight("Speck")
+
+    c = AES_Crypt()
+
+    fin = open("test_file_pt.txt", "rb")
+    fout1 = open("test_file_ct.txt", "wb")
+    fout2 = open("test_file_rpt.txt", "wb")
+    data = fin.read() 
+
+    # start = time.time()
+    # for i in range(0, len(data), 100):
+    #     ct = c.encrypt(data[i:i+100])
+    #     fout1.write(ct)
+    #     rpt = c.decrypt(ct)
+    #     fout2.write(rpt)
+    # end = time.time()
+    # print("Speck Time: ", end - start)
+
+    start = time.time()
+    for i in range(0, len(data), 100):
+        iv, ct = c.encrypt(data[i:i+100])
+        fout1.write(ct)
+        rpt = c.decrypt(iv, ct)
+        fout2.write(rpt)
+    end = time.time()
+    print("AES Time: ", end-start)
+
+
+    fin.close()
+    fout1.close()
+    fout2.close()
+
+
 if __name__ == "__main__":
     main()
